006abd133611b22cd1341c3ed2fc9c90addeedd4
Zbyszek212322 <zbyszrad@gmail.com>
2020-06-05 23:51:18 +0200

    trimming of classes

--- src/main/java/com/example/demo/RunAtStart.java
+++ src/main/java/com/example/demo/RunAtStart.java
@@ -7,19 +7,12 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 @Component
 public class RunAtStart {
-
-    @Autowired
-    private EmployeeRepository employeeRepository;
-
-    @Autowired
-    private DepartmentRepository departmentRepository;
 
     @Autowired
     private RoleRepository roleRepository;
@@ -44,20 +37,6 @@ public class RunAtStart {
 
     @PostConstruct
     public void runAtStart() {
-
-        Department dept1 = departmentRepository.save(new Department("Finances"));
-        Department dept2 = departmentRepository.save(new Department("Production"));
-
-        Employee emp1 = employeeRepository.save(new Employee(1L, "Euzebiusz", "Nowacki", new BigDecimal("4000.50"),
-                LocalDate.of(1999, 1, 1), dept1));
-        Employee emp2 = employeeRepository.save(new Employee(2L, "Eugeniusz", "Nowak", new BigDecimal("1000.00"),
-                LocalDate.of(1997, 1, 1), dept2));
-        Employee emp3 = employeeRepository.save(new Employee(3L, "Jan", "Wiśniewski", new BigDecimal("2500.00"),
-                LocalDate.of(1998, 1, 1), dept2));
-        Employee emp4 = employeeRepository.save(new Employee(4L, "Kamil", "Wiśniewski", new BigDecimal("2800.00"),
-                LocalDate.of(2001, 1, 1), dept2));
-
-        // ---------------------------------------------------------------------------------------------------------
 
         Role role1 = roleRepository.save(new Role(EnumRole.ROLE_USER));
         Role role2 = roleRepository.save(new Role(EnumRole.ROLE_ADMIN));
